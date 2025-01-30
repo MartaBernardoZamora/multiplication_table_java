8d7cc6ee104f43faff4f4d24b3114a177d18e336
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-30 19:32:34 +0100

    create product test for two numbers and fail

---
+++ src/main/java/dev/martabernardo/multiplication_table/multiplicationtable/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package dev.martabernardo.multiplication_table.multiplicationtable;
+
+public class MultiplicationTable {
+
+    public MultiplicationTable(int factor1) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public int multiply(int factor2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'multiply'");
+    }
+
+}
---
+++ src/test/java/dev/martabernardo/multiplication_table/multiplicationtable/MultiplicationTableTest.java
@@ -0,0 +1,26 @@
+package dev.martabernardo.multiplication_table.multiplicationtable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MultiplicationTableTest {
+
+    @Test
+    @DisplayName("Test devuelve producto de un número por otro")
+    void testReturnsProductNumberToNumber() {
+        int factor1 = 3;
+
+        MultiplicationTable multiplicationTable = new MultiplicationTable(factor1);
+        
+        int factor2 = 2;
+        int resultExpected = 6;
+        int result = multiplicationTable.multiply(factor2);
+
+        assertEquals(resultExpected, result);
+        
+    }
+
+
+}
