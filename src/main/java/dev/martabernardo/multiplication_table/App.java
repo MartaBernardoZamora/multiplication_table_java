65eddad50e4c6e9da2d8bc3e52e23c71c2d2f352
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-30 16:14:41 +0100

    initial commit

---
+++ src/main/java/dev/martabernardo/multiplication_table/App.java
@@ -0,0 +1,10 @@
+package dev.martabernardo.multiplication_table;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/dev/martabernardo/multiplication_table/AppTest.java
@@ -0,0 +1,12 @@
+package dev.martabernardo.multiplication_table;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
