3b8aa069234132014a0b57f697bdf53a0c8d4434
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-30 18:46:21 +0100

    create main test to App.main general overall and ok status

--- src/test/java/dev/martabernardo/multiplication_table/AppTest.java
+++ src/test/java/dev/martabernardo/multiplication_table/AppTest.java
@@ -2,11 +2,12 @@ package dev.martabernardo.multiplication_table;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
 
 class AppTest {
-    @Test
-    void testApp() {
-        assertEquals(1, 1);
+    @Test 
+    @DisplayName("Test app functionality") 
+    void testMain() { 
+        App.main(new String[]{}); 
     }
 }
