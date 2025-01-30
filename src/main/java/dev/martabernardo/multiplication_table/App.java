6fee2ccae8005b76f3bea407c941d49b5b537524
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-30 20:28:40 +0100

    create iteration result test and fail in App class

--- src/main/java/dev/martabernardo/multiplication_table/App.java
+++ src/main/java/dev/martabernardo/multiplication_table/App.java
@@ -7,4 +7,9 @@ public final class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static String iteration(int num, int numIter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iteration'");
+    }
 }
--- src/test/java/dev/martabernardo/multiplication_table/AppTest.java
+++ src/test/java/dev/martabernardo/multiplication_table/AppTest.java
@@ -2,6 +2,8 @@ package dev.martabernardo.multiplication_table;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 
 class AppTest {
@@ -10,4 +12,19 @@ class AppTest {
     void testMain() { 
         App.main(new String[]{}); 
     }
+
+    @Test
+    @DisplayName("Test return correct result in iteration")
+    void testName() {
+        int num=5;
+        int numIter=1;
+
+        String result= App.iteration(num, numIter);
+
+        String resultExpected="5 x 1 = 5";
+        assertEquals(resultExpected, result);
+
+
+    }
+    
 }
